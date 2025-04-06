f086b228e2266b80b3de6b94087b7a56d9debf52
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2025-04-06 19:35:34 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,12 +19,15 @@ const Home = () => (
       <p className="text-lg mb-6 text-gray-300 mt-2">
         Discover, manage, and enjoy events like never before. Your ultimate event management solution is here!
       </p>
+      <link to"/events">
       <a
         href="/events"
         className="py-2 px-4 rounded-md bg-gradient-to-r from-indigo-500 to-purple-500 hover:from-indigo-600 hover:to-purple-600 transition"
       >
         Discover Events
       </a>
+      </Link>
+      </div>
     </div>
   </div>
 );
