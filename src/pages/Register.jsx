54a0a52482e60bc316e92be9e4bfb4852f813702
C Bhuvaneshvar Reddy <bhuvaneshvar.r.c@gmail.com>
2025-04-06 17:55:39 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('https://event-backend-utqn.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 
           "Content-Type": "application/json" 
